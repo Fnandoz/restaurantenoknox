e8611e91137b67e679f4e6618792a505f4e9b07f
Fernando Gomes <lfgsnando@gmail.com>
2019-07-12 16:29:38 -0300

    implement api

--- Restaurante Noknox/Network/RestApi.swift
+++ Restaurante Noknox/Network/RestApi.swift
@@ -13,26 +13,39 @@ import ObjectMapper
 protocol RestApiProtocol {
     static func makeLogin(username: String, password: String, callback: @escaping(_ auth: Auth?) -> Void, error: @escaping() -> Void)
     
-    static func getCuisine(lat: Double, lng: Double, auth: Auth, callback: @escaping(_ cuisines: [Cuisine]) -> Void, error: @escaping() -> Void)
+    static func getCuisine(lat: Double, lng: Double, authToken: String, callback: @escaping(_ cuisines: [Cuisine]) -> Void, error: @escaping() -> Void)
     
-    static func getRestaurant(lat: Double, lng: Double, cuisines: [Cuisine], callback: @escaping(_ restaurants: [Restaurant]) -> Void, error: @escaping() -> Void)
+    static func getRestaurant(lat: Double, lng: Double, cuisines: String, authToken: String, callback: @escaping(_ restaurants: [Restaurant]) -> Void, error: @escaping() -> Void)
 }
 
 class RestApi: RestApiProtocol {
     static func makeLogin(username: String, password: String, callback: @escaping (Auth?) -> Void, error: @escaping () -> Void) {
         let url = Constants.BaseUrl + "/token"
-        Alamofire.request(url, method: .post, parameters: nil, encoding: URLEncoding.default, headers: ["Content-Type": "application/json"]).validate().responseString { (response: DataResponse<String>) in
+        Alamofire.request(url, method: .post, parameters: ["grant_type":"password", "username":username, "password":password], encoding: URLEncoding.default, headers: ["Content-Type": "application/json"]).validate().responseString { (response: DataResponse<String>) in
             let auth = Mapper<Auth>().map(JSONString: response.value!)
             callback(auth)
         }
     }
     
-    static func getCuisine(lat: Double, lng: Double, auth: Auth, callback: @escaping ([Cuisine]) -> Void, error: @escaping () -> Void) {
+    static func getCuisine(lat: Double, lng: Double, authToken: String, callback: @escaping ([Cuisine]) -> Void, error: @escaping () -> Void) {
+        let url = Constants.BaseUrl + "/cuisine?lat=\(lat)&lng=\(lng)"
+        Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers:
+            ["Content-Type": "application/json", "Authorization":"Bearer "+authToken]).validate().responseString {
+                (response: DataResponse<String>) in
+                let cuisines = Mapper<Cuisine>().mapArray(JSONfile: response.value!)
+                callback(cuisines!)
+        }
         
     }
     
-    static func getRestaurant(lat: Double, lng: Double, cuisines: [Cuisine], callback: @escaping ([Restaurant]) -> Void, error: @escaping () -> Void) {
+    static func getRestaurant(lat: Double, lng: Double, cuisines: String, authToken: String, callback: @escaping ([Restaurant]) -> Void, error: @escaping () -> Void) {
+        let url = Constants.BaseUrl + "/restaurant?lat=\(lat)&lng=\(lng)&cuisines=\(cuisines)"
         
+        Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: ["Content-Type": "application/json", "Authorization":"Bearer "+authToken]).validate().responseString {
+            (response: DataResponse<String>) in
+            let restaurants = Mapper<Restaurant>().mapArray(JSONfile: response.value!)
+            callback(restaurants!)
+        }
     }
     
     
